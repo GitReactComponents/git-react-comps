f2c98d7f2999f0d5ba45a0cab9e712ac8f3dd66e
Wade Pate <wadejp8@gmail.com>
2021-03-19 16:52:24 -0600

    began header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import './App.css';
+import Header from './Components/Header/Header'
+
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
+      
+      <Header />
 
     </div>
   );
