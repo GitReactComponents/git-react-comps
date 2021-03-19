21b303e0523957d5033d7eb5bf17e6d638ff9265
Wade Pate <wadejp8@gmail.com>
2021-03-19 11:29:56 -0600

    added some endpoints

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 require("dotenv").config();
 const massive = require("massive");
+const express = require("express"),
+  ctrlComp = require("./controllers/comps"),
+  ctrlPost = require("./controllers/posts"),
+  ctrlUser = require("./controllers/user"),
+  auth = require("./middleware/authCheck");
 const session = require("express-session");
-const express = require("express");
 
 const { CONNECTION_STRING, SERVER_PORT, SESSION_SECRET } = process.env;
 
@@ -27,3 +31,13 @@ massive({
     console.log(`Server listening on port ${SERVER_PORT}`);
   });
 });
+
+
+// ! comps endpoints
+app.get('/api/comp', ctrlComp.readComp)
+
+// ! posts endpoints
+app.get('/api/posts', ctrlPost.readPosts)
+
+// ! user endpoints
+app.post('/api/auth/register', ctrlUser.register)
