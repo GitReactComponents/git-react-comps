8d491e797f200ee53aa7b38fe4d69a6ba54955a7
Junior Ibarra <Ibarra_0811@yahoo.com>
2021-03-19 15:23:00 -0700

    CompContext

---
+++ src/Context/CompContext.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const CompContext = React.createContext()
+
+const CompProvider = CompContext.Provider
+const CompConsumer = CompContext.Consumer
+
+export { CompProvider, CompConsumer }
