ad1da3a9f73cafcb7b1fb2466b66143db09f9ff1
Wade Pate <wadejp8@gmail.com>
2021-03-22 15:18:34 -0600

    added reqs for ref to Subscribe

--- src/Components/Views/Subscribe.js
+++ src/Components/Views/Subscribe.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
 function Subscribe() {
+
+
+
   return (
     <div className='subscribe'>
       This is the Subscribe Page
+      <br/>
+      Enter your Username, email, first name, last name, and a password!
     </div>
   )
 }
