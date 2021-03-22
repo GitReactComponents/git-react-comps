7b0a92b2463c23aba4fca5cbb699777549f41b28
Austin Reece <austin.m.reece@gmail.com>
2021-03-22 12:18:15 -0700

    Updated Auth Context

--- src/Context/AuthContext.js
+++ src/Context/AuthContext.js
@@ -1,14 +1,48 @@
 import { createContext, useState } from 'react'
+import { useHistory } from 'react-router-dom'
 import axios from 'axios'
 
 export const AuthContext = createContext(null)
 
 export const AuthProvider = (props) => {
     const [user, setUser] = useState(null)
+    const { push } = useHistory('')
 
     //axios calls go here
     const getUser = () => {
         axios.get('/api/user')
+            .then(({ data }) => {
+                setUser(data)
+            })
+    }
+    
+    const register = () => {
+        axios.post('/api/auth/register', { userName, email, firstName, lastName, password })
+        .then(({data}) => {
+            setUser(data)
+            push('/')
+        })
+    }
+
+    const login = () => {
+        axios.post('/auth/login', { userName, password })
+            .then(({ data }) => {
+                setUser(data)
+                push('/')
+            })
+            .catch(err => console.log(err))
+    }
+
+    const logout = () => {
+        axios.post('/api/logout')
+        .then(() => {
+            setUser('')
+            push('/')
+        })
+    }
+
+    const updateUser = () => {
+        axios.put('/api/edit_user', { userId, isMember: true })
         .then()
     }
 
