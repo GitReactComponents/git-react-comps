350645301f1e41597a6d8c8e3dfce0e1b36aed51
Junior Ibarra <Ibarra_0811@yahoo.com>
2021-03-23 08:15:09 -0700

    partial compContext

--- src/Context/CompContext.js
+++ src/Context/CompContext.js
@@ -5,13 +5,43 @@ import axios from 'axios'
 export const CompContext = createContext(null)
 
 export const CompProvider = (props) => {
-    const [component, setComponent] = useState({})
+  const [component, setComponent] = useState({})
 
+  // app.get('/api/posts-nm', ctrlPost.readPosts)
+const getNmComps = () => {
+  axios.get('./api/comp-nm').then((res) => {
+    setComponent(res.data)
+  }).catch(err => console.log(err))
+}
 
+// app.get('/api/member-comp', auth.userOnly, ctrlComp.readAllComp)
+const getMemberPosts = () => {
+  axios.get('./api/member-comp', {}).then(({}) => {
 
-    return (
-        <CompContext.Provider value={{component, }}>
+  }).catch()
+} 
+
+// app.post('/api/create-comp', auth.userOnly, ctrlComp.createComp)
+const createComp = () => {
+  axios.post('/api/create-comp').then(() => {
+    })
+  } 
+
+// app.put('/api/personal-post/:compId', auth.userOnly, ctrlComp.editComp)
+const personalPost = () => {
+  axios.put(`/api/personal-post/:compId`, {}).then(({}) => {
+  })
+} 
+
+// app.delete('/api/personal-post/:compId', auth.userOnly, ctrlComp.deleteComp)
+const deletePersonalPost = (compId) => {
+  axios.delete(`/api/personal-post/:compId`).then(() => {
+  }).catch(err => console.log(err))
+} 
+
+  return (
+      <CompContext.Provider value={{ component, }}>
             {props.children}
-        </CompContext.Provider>
-    )
+      </CompContext.Provider>
+  )
 }
