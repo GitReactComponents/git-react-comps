bb22b2504c25a7d341afa9635ae0e69f1a5a8e94
Austin Reece <austin.m.reece@gmail.com>
2021-03-24 09:59:37 -0700

    completed comp controller and comp db queries

--- src/Context/CompContext.js
+++ src/Context/CompContext.js
@@ -1,4 +1,4 @@
-import {createContext, useState} from 'react'
+import { createContext, useState } from 'react'
 import axios from 'axios'
 
 
@@ -7,38 +7,39 @@ export const CompContext = createContext(null)
 export const CompProvider = (props) => {
   const [component, setComponent] = useState({})
 
-// app.get('/api/comp-nm', ctrlComp.readComp)
-const getNmComps = () => {
-  axios.get('/api/comp-nm').then((res) => {
-    setComponent(res.data)
-  }).catch(err => console.log(err))
-}
+  // app.get('/api/comp-nm', ctrlComp.readComp)
+  const getNmComps = () => {
+    axios.get('/api/comp-nm').then((res) => {
+      setComponent(res.data)
+    }).catch(err => console.log(err))
+  }
 
-// app.get('/api/member-comp', auth.userOnly, ctrlComp.readAllComp)
-const getMemberComp = () => {
-  axios.get('/api/member-comp').then((res) => {
-    setComponent(res.data)
-  }).catch(err => console.log(err))
-} 
+  // app.get('/api/member-comp', auth.userOnly, ctrlComp.readAllComp)
+  const getMemberComp = () => {
+    axios.get('/api/member-comp').then((res) => {
+      setComponent(res.data)
+    }).catch(err => console.log(err))
+  }
 
-// app.post('/api/create-comp', auth.userOnly, ctrlComp.createComp)
-const createComp = (pic, title, code, desc) => {
-  axios.post('/api/create-comp', {pic, title, code, desc}).then(({data}) => {
-    setComponent(data)
-  }).catch(err => console.log(err))
-  } 
+  // app.post('/api/create-comp', auth.userOnly, ctrlComp.createComp)
+  const createComp = (component_type, component_image, component_info, userId, username) => {
+    axios.post('/api/create-comp', { component_type, component_image, component_info, userId, username })
+      .then(({ data }) => {
+        setComponent(data)
+      }).catch(err => console.log(err))
+  }
 
-// app.delete('/api/delete-comp/:compId', auth.userOnly, ctrlComp.deleteComp)
-const deleteComp = (compId) => {
-  axios.delete(`/api/delete-comp/:compId`).then(res => {
-    console.log(res.data)
-    setComponent(res.data)
-  }).catch(err => console.log(err))
-} 
+  // app.delete('/api/delete-comp/:compId', auth.userOnly, ctrlComp.deleteComp)
+  const deleteComp = (component_id) => {
+    axios.delete(`/api/delete-comp/:${component_id}`).then(res => {
+      console.log(res.data)
+      setComponent(res.data) // ? not usre about this line. Should we be setting the componet sate with res.data here?
+    }).catch(err => console.log(err))
+  }
 
   return (
-      <CompContext.Provider value={{ component, }}>
-            {props.children}
-      </CompContext.Provider>
+    <CompContext.Provider value={{ component, }}>
+      {props.children}
+    </CompContext.Provider>
   )
 }
