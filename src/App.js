11a634eed4ec9445f2f8a8e8da107c10a63e12cf
Austin Reece <austin.m.reece@gmail.com>
2021-03-25 11:43:28 -0700

    embed demo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import routes from './Routes'
 import Header from './Components/Header/Header'
+import CodepeEmbed from './Components/Codepen/CodepenEmbed'
 
 import './App.scss';
 import {BrowserRouter as Route, Switch} from 'react-router-dom';
@@ -9,6 +10,7 @@ function App() {
   return (
     <div>
       <Header />
+      <CodepeEmbed />
       <Route>
         <Switch>
           {routes}
