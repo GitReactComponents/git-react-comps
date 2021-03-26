65d1d738dd4e84ef55c782174dd1a629931e5c36
Josh Hyink <joshuabhyink@gmail.com>
2021-03-26 16:42:08 -0600

    stash changes

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -5,33 +5,28 @@ module.exports = {
             res.status(200).send(posts)
         })
     },
-    readPost: (req, res) => {
-        const db = req.app.get('db')
-        db.post_db.get_post_by_id(component_id).then(post => {
-            res.status(200).send(post)
-        })
-    },
     createPost: (req, res) => {
         const db = req.app.get('db')
         const {postText} = req.body
-        db.post_db.add_post(postText).then(post => {
+        const {userId, userName} = req.session.user
+        db.post_db.add_post(postText, userId, userName).then(post => {
             res.status(200).send(post)
         })
     },
-    editPost: (req, res) => {
-        const db = req.app.get('db')
-        const {postText, postId} = req.body
-        const {userId} = req.session.user
-        db.post_db.edit_post(postId, userId, postText).then(post => {
-            res.status(200).send(post)
-        })
-    }, 
+    // editPost: (req, res) => {
+    //     const db = req.app.get('db')
+    //     const {postText, postId} = req.body
+    //     const {userId} = req.session.user
+    //     db.post_db.edit_post(postId, userId, postText).then(post => {
+    //         res.status(200).send(post)
+    //     })
+    // }, 
     deletePost: (req, res) => {
         const db = req.app.get('db')
         const {postId} = req.body
         const {userId} = req.session.user
-        db.post_db.delete_post(postId, userId).then(posts => {
-            res.status(200).send(posts)
+        db.post_db.delete_post(postId, userId).then(_ => {
+            res.sendStatus(200)
         })
     }
 }
