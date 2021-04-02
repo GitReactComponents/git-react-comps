446f1a3a784121e849b45948455856fb9f9a4577
Austin Reece <austin.m.reece@gmail.com>
2021-04-02 09:18:23 -0700

    saving changes and merging

--- src/Components/Views/Home.js
+++ src/Components/Views/Home.js
@@ -1,23 +1,25 @@
 import React, {useState, useContext} from 'react'
-import ReactSpringBtn from '../CompDisplay/ReactSpringBtn'
-import ComponentCategories from '../CompDisplay/ComponentCategories'
+// import ReactSpringBtn from '../CompDisplay/ReactSpringBtn'
+// import ComponentCategories from '../CompDisplay/ComponentCategories'
 import CategoryTile from '../CompDisplay/CategoryTile'
-
+import {AuthContext} from '../../Context/AuthContext'
 
 
 function Home() {
   const [categories, setCategories] = useState(['Button', 'Header', 'Footer', 'Navbar', 'Form', 'Card', 'List', 'Menu', 'Search', 'Tabs'])
-  
+  const authContext = useContext(AuthContext)
 
   return (
-    <div className='home'>
-      Check Out Our Components By Selecting a Category
-      <div>
-        {categories.map((category) => {
-          return <CategoryTile category={category}/>
-        })}
+   
+      <div className='home'>
+        Check Out Our Components By Selecting a Category
+        <div>
+          {categories.map((category) => {
+            return <CategoryTile category={category}/>
+          })}
+        </div>
       </div>
-    </div>
+    
   )
 }
 
